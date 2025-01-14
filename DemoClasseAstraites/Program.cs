ab221ee32060a63e13f947f9641994bb4a33c8ef
Khun <khun.ly@bstorm.be>
2025-01-14 09:24:03 +0100

    abstract

---
+++ DemoClasseAstraites/Program.cs
@@ -0,0 +1,51 @@
+﻿Carre c = new Carre { Longueur = 1.5 };
+Forme cercle = new Cercle { Rayon = 2 };
+
+// on ne peut pas instancier une classe abtraites car elle est incomplète
+// Forme f = new Forme();
+
+
+Console.WriteLine(c.CalculerAire()); //2.25
+Console.WriteLine(cercle.CalculerAire()); //12.56
+
+
+abstract class Forme
+{
+    public string Color { get; set; }
+
+    // CalculerAire est une methode abstraites
+    // pas encore défine
+    // il appartiendra aux classes enfants d'implémenter cette méthode 
+    public abstract double CalculerAire();
+}
+
+class Cercle : Forme
+{
+    public double Rayon { get; set; }
+
+    public override double CalculerAire()
+    {
+        return Math.PI * Rayon * Rayon;
+    }
+}
+
+
+class Carre : Forme
+{
+    public double Longueur { get; set; }
+
+    public override double CalculerAire()
+    {
+        return Longueur * Longueur;
+    }
+}
+
+class Rectangle : Forme
+{
+    public double Longueur { get; set; }
+    public double Largeur { get; set; }
+    public override double CalculerAire()
+    {
+        return Largeur * Longueur;
+    }
+}
---
+++ DemoHeritage/Program.cs
@@ -0,0 +1,73 @@
+﻿// polymorphisme des classes
+Animal c = new Chat();
+c.Nom = "Felix";
+c.DateNaissance = DateTime.Now;
+c.DireBonjour();
+c.Test();
+
+//cast explicite
+((Chat)c).Miauler();
+
+Animal c1 = new Chien()
+{
+    Nom = "Pluto"
+};
+c1.DireBonjour();
+// ne fonctionne pas
+// ((Chat)c1).Miauler();
+
+
+class Animal
+{
+    public string Nom { get; set; }
+    public DateTime DateNaissance { get; set; }
+    public virtual void DireBonjour()
+    {
+        Console.WriteLine($"Hello, je m'appelle {Nom}");
+    }
+
+    public void Test()
+    {
+        Console.WriteLine("test");
+    }
+}
+
+class Chat : Animal
+{
+    public void Miauler()
+    {
+        Console.WriteLine("MIAOU");
+    }
+
+    public override void DireBonjour()
+    {
+        Console.WriteLine("MIAOU");
+        base.DireBonjour();
+    }
+
+    // a eviter
+    public new void Test()
+    {
+        Console.WriteLine("test chat");
+    }
+}
+
+class Siamois : Chat
+{
+    public override void DireBonjour()
+    {
+        base.DireBonjour();
+    }
+}
+
+class Chien : Animal
+{
+    public override void DireBonjour()
+    {
+        Console.WriteLine("Waf waf");
+    }
+    public void Aboyer()
+    {
+        Console.WriteLine("WAF");
+    }
+}
