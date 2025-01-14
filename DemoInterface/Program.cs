3d2e0a170e0e3bf40e3db2def9a07f4b5c838ade
Khun <khun.ly@bstorm.be>
2025-01-14 12:56:00 +0100

    interfaces

---
+++ DemoInterface/Program.cs
@@ -0,0 +1,74 @@
+﻿Secretaire monique = new Secretaire
+{
+    Nom = "monique",
+    Telephone = new SamsungGalaxy()
+};
+
+monique.FaireUneCommande();
+
+class VieuxNokia : ITelephone
+{
+    public List<string> Contacts { get; }
+
+    public void ConsulterMails()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EnvoyerSMS()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Telephonner(string numero)
+    {
+        Console.WriteLine("Je telephone avec un Vieux Nokia");
+    }
+}
+class SamsungGalaxy : ITelephone
+{
+    public List<string> Contacts { get; }
+
+    public void ConsulterMails()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EnvoyerSMS()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Telephonner(string numero)
+    {
+        Console.WriteLine("J'appelle depuis un Samsung Galaxy");
+    }
+
+    public void Telephonner(int numero)
+    {
+        Console.WriteLine("J'appelle depuis un Samsung Galaxy");
+    }
+}
+
+
+interface ITelephone
+{
+    const int value = 4;
+    List<string> Contacts { get; }
+    void Telephonner(string numero);
+    void EnvoyerSMS();
+    void ConsulterMails();
+}
+
+
+class Secretaire
+{
+    public string Nom { get; set; }
+
+    public ITelephone Telephone { get; set; }
+
+    public void FaireUneCommande() 
+    {
+        Telephone.Telephonner("0476/666666");
+    }
+}
---
+++ DemoStatique/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+Chat c1 = new Chat { Couleur = "Noir", Nom = "Felix" };
+Chat c2 = new Chat { Couleur = "Roux", Nom = "Steve" };
+
+Chat.Race = "Siamois";
+
+c1.SePresenter();
+c2.SePresenter();
+
+Chat.Race = "De goutières";
+
+c1.SePresenter();
+c2.SePresenter();
+
+Console.WriteLine(Math.Pow(4, 5));
+
+c1.Manger();
+c2.Manger();
+
+int[] t = [1, 2, 5];
+List<int> l = [1, 2, 3];
+int value = l.First(i => i > 2);
+int value2 = t.First(i => i > 2);
+
+Console.WriteLine(c1);
+
+class Chat
+{
+    public string Nom { get; set; }
+    public string Couleur { get; set; }
+
+    public static string Race { get; set; }
+
+    public void SePresenter()
+    {
+        Console.WriteLine($"Je m'appelle {Nom}, je suis {Couleur} et Ma race est {Race}");
+    }
+
+    public override string ToString()
+    {
+        return Nom;
+    }
+}
+
+static class ChatExtensions
+{
+    public static void Manger(this Chat c)
+    {
+        Console.WriteLine($"{c.Nom} est en train de manger");
+    }
+}
+
